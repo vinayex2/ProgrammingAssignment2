09dfc23eda1281145faea3ca5bf0d51a1007ede6
vinayex2 <vinayex2@gmail.com>
2015-02-21 19:06:35 +0530

    Comments Added

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,9 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## MakeCacheMatrix function takes a matrix as input and sets Up getter and Setter
+## functions for the data and the Matrix Inverse
+## Uses <<- operator for pushing data in the variables to enable Caching
 makeCacheMatrix <- function(x = matrix()) {
   matrixInverse<-NULL
   set<- function(y)
@@ -24,6 +26,10 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+## The function takes a Matrix as input , checks if Inverse has been set by
+## previous operations using is.Null Check.
+## Gets data from saved Cache variable if unchanged matrix is passed again
+## Returns Message "Getting from Cache" if retrieved from Cache was successful
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -33,8 +39,7 @@ cacheSolve <- function(x, ...) {
     message("Getting from cache")
     return(inverse)
     
-  }
-  
+  }  
   matrix<- x$get()
   inverse<-solve(matrix, ...)
   x$setInverse(inverse)
